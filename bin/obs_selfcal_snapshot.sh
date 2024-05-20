d4f44c46e1a8e25f6c977bf6a856f06ffb04d294
silviamantovanini <silvia.mantovanini@postgrad.curtin.edu.au>
2024-05-20 13:44:55 +0800

    selfcal adjustments

--- bin/obs_selfcal_snapshot.sh
+++ bin/obs_selfcal_snapshot.sh
@@ -105,9 +105,9 @@ chmod 755 "${script}"
 
 # sbatch submissions need to start with a shebang
 echo '#!/bin/bash' > ${script}.sbatch
-echo "srun --cpus-per-task=${GXNCPUS} --ntasks=1 --ntasks-per-node=1 singularity run ${GXCONTAINER} ${script}" >> ${script}.sbatch
+echo "srun --cpus-per-task=${GXNCPUS} --ntasks=1 --ntasks-per-node=1 singularity run -B /nvmetmp/ ${GXCONTAINER} ${script}" >> ${script}.sbatch
 
-sub="sbatch --begin=now --export=ALL  --time=12:00:00 --mem=${GXABSMEMORY}G -M ${GXCOMPUTER} --output=${output} --error=${error}"
+sub="sbatch --begin=now --export=ALL  --time=12:00:00 --mem=${GXABSMEMORY}G -M ${GXCOMPUTER} --tmp=60GB --output=${output} --error=${error}"
 sub="${sub} ${GXNCPULINE} ${account} ${GXTASKLINE} ${jobarray} ${depend} ${queue} ${script}.sbatch"
 if [[ ! -z ${tst} ]]
 then
